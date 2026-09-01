f4dc10ffa8ffdef41e4ad8722b5591900d342fd1
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-01 09:31:18 +0200

    fix: correct 64-bit Murim core constant

--- engine/murim_core.c
+++ engine/murim_core.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 uint64_t murim_mix64(uint64_t x){
   x ^= x >> 30; x *= UINT64_C(0xbf58476d1ce4e5b9);
-  x ^= x >> 27; x *= UINT64_C(0x94d049bb133111eb9);
+  x ^= x >> 27; x *= UINT64_C(0x94d049bb133111eb);
   return x ^ (x >> 31);
 }
 uint32_t murim_seed_for(uint64_t person_id, uint64_t world_seed){
