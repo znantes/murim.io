c24199aebd2d732bf20b0ac6ae2a9d6aaa328b75
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-03 22:54:18 +0200

    Add inherited NPC predisposition model

---
+++ src/World/InheritanceProfile.cs
@@ -0,0 +1,10 @@
+namespace Murim.World;
+
+public sealed class InheritanceProfile
+{
+    public double PhysicalPotential { get; set; }
+    public double MentalPotential { get; set; }
+    public double InternalEnergyPotential { get; set; }
+    public double RecoveryPotential { get; set; }
+    public double LearningPotential { get; set; }
+}
