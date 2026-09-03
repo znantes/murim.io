6b9b70c13a34b7a64329ad505ad72e5dc0f6fe3f
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-03 22:53:20 +0200

    Connect birth generation to world state

--- src/World/WorldState.cs
+++ src/World/WorldState.cs
@@ -7,9 +7,20 @@ public sealed class WorldState
     public GameTime Time { get; } = new();
     public Dictionary<Guid, Npc> Npcs { get; } = new();
 
+    public Npc? PlayerNpc { get; private set; }
+
     public void AddNpc(Npc npc)
     {
         ArgumentNullException.ThrowIfNull(npc);
         Npcs[npc.Id] = npc;
+    }
+
+    public Npc CreatePlayerAtBirth(int seed, string familyName = "Unknown")
+    {
+        var generator = new BirthGenerator(seed);
+        var npc = generator.CreateNewborn(familyName);
+        AddNpc(npc);
+        PlayerNpc = npc;
+        return npc;
     }
 }
