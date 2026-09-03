e3d30303cacf1cbe4d5a5a229d3dbff08fb42350
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-03 22:52:54 +0200

    Add NPC mind model

---
+++ src/World/Mind.cs
@@ -0,0 +1,12 @@
+namespace Murim.World;
+
+public sealed class Mind
+{
+    public double Intelligence { get; set; }
+    public double Memory { get; set; }
+    public double Concentration { get; set; }
+    public double Willpower { get; set; }
+    public double Perception { get; set; }
+    public double LearningAbility { get; set; }
+    public double MentalResilience { get; set; }
+}
