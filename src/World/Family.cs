7efb6d60a7c5c536468cfcd758a03db03e8b8bb5
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-03 23:06:46 +0200

    B19 - Add family origin and multigenerational genealogy links

--- src/World/Family.cs
+++ src/World/Family.cs
@@ -1,10 +1,26 @@
 namespace Murim.World;
 
+public enum FamilyOrigin
+{
+    Common,
+    Imperial,
+    Noble,
+    Martial,
+    Merchant,
+    Religious,
+    Criminal,
+    Secretive,
+    Rural
+}
+
 public sealed class Family
 {
     public Guid Id { get; } = Guid.NewGuid();
     public string Name { get; set; } = "Unknown Family";
+    public FamilyOrigin Origin { get; set; } = FamilyOrigin.Common;
+    public string SocialStatus { get; set; } = "Common";
     public Guid? FatherId { get; set; }
     public Guid? MotherId { get; set; }
+    public Guid? ParentFamilyId { get; set; }
     public List<Guid> ChildrenIds { get; } = new();
 }
