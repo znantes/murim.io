8e0c91011d45e5e4e1423ee2e47b99ee16dea0d8
bg dffvddsdvc <venceslas--44@hotmail.com>
2026-09-08 15:58:41 +0200

    Web: resolve Blazor root component namespace

--- web-client/Program.cs
+++ web-client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Murim.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
